fbba36c4d305861ffd9d354d3b623e96485c71f6
mostafa <mustmustapha67@gmail.com>
2026-08-09 14:14:46 +0100

    add classes

--- parser.py
+++ parser.py
@@ -8,3 +8,17 @@ for line in lines:
     line = line.strip()
 
     print(line)
+    try:
+        if line.startswith("nb_drones"):
+            key , value = line.split(":", 1)
+            print(f"key: {key} --- value:{value}")
+            nb_drones = int(value.strip())
+            print(nb_drones)
+        elif line.startswith("start_hub"):
+            ...
+
+    except:
+        print("not found")
+
+
+# nb_drones: 5
